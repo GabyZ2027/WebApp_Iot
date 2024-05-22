aa648606998cf1503204bcd93d1f3c4fbc5b3f93
Marti <marti.rafart@estudiantat.upc.edu>
2024-05-22 09:30:12 +0200

    Fet servei de monitoratge

--- webapp-iot/app/src/routes/api.py
+++ webapp-iot/app/src/routes/api.py
@@ -1,62 +1,54 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Blueprint
 from src.models.kafka import KafkaManager
-from src.models.database import Sensors_BD
+from src.models.S_database import Sensors_BD
 
-app = Flask(__name__)
-"""
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
 
-class SensorData(db.Model):
-    __tablename__ = 'SENSORS'
-    sensor = db.Column(db.String, primary_key=True)
-    data = db.Column(db.Date, primary_key=True)
-    lectura = db.Column(db.String, nullable=False)
-"""
+api_blueprint = Blueprint('api_blueprint',__name__)
+
+kafka_manager = KafkaManager("172.17.0.3")
+sensors_bd = Sensors_BD('sensors.db', max=5000)
 
 @app.route('/sensor/temperatura/historial', methods=['GET'])
 def temperatura_historial():
-    lecturas = sensors_bd.getLecturesSen(10)
-    return jsonify({"lecturas": lecturas})
+    temperatura_historial = sensors_bd.getLectures(1,10)
+    return jsonify({"temps": temperatura_historial[0],"data": temperatura_historial[1]})
 
 @app.route('/sensor/humitat/historial', methods=['GET'])
 def humitat_historial():
-    lecturas = sensors_bd.getLecturesSen(10)
-    return jsonify({"lecturas": lecturas})
+    humitat_historial = sensors_bd.getLectures(2,10)
+    return jsonify({"temps": humitat_historial[0],"data": humitat_historial[1]})
 
 @app.route('/led/historial', methods=['GET'])
 def led_historial():
-    lecturas = sensors_bd.getLecturesAct(10)
-    return jsonify({"lecturas": lecturas})
+    led_historial = sensors_bd.getLectures(0,10)
+    return jsonify({"temps": led_historial[0],"data": led_historial[1]})
 
-@app.route('/sensor/humitat/', methods=['GET'])
+@app.route('/sensor/humitat', methods=['GET'])
 def humitat():
-    lectura = sensors_bd.getLecturaSen()
-    return jsonify({"lectura": lectura})
+    humitat = sensors_bd.getLectura(2)
+    return jsonify({"temps": humitat[0],"data": humitat[1]})
+
 
 @app.route('/sensor/temperatura', methods=['GET'])
 def temperatura():
-    lectura = sensors_bd.getLecturaAct()
-    return jsonify({"lectura": lectura})
+    temperatura = sensors_bd.getLectura(1)
+    return jsonify({"temps": temperatura[0],"data": temperatura[1]})
 
-@app.route('/led', methods=['GET'])
-def led_lectura():
-    lectura = sensors_bd.getLecturaAct()
-    return jsonify({"lectura": lectura})
 
-@app.route('/led', methods=['POST'])
-def control_led():
-    data = request.get_data(as_text=True)
-    if "led-status=1" in data:
-        kafka_manager.produce("led", "1")
-        return "LED ON"
-    elif "led-status=0" in data:
-        kafka_manager.produce("led", "0")
-        return "LED OFF"
-    else:
-        return "Mensaje no reconocido"
+@app.route('/led', methods=['GET', 'POST'])
+def led():
+    if request.method == 'GET':
+        led = sensors_bd.getLectura(0)
+        return jsonify({"temps": led[0],"data": led[1]})
+    
+    elif request.method == 'POST':
+        data = request.get_data(as_text=True)
+        if "led-status=1" in data:
+            kafka_manager.produce("led", "1")
+            return "LED ON"
+        elif "led-status=0" in data:
+            kafka_manager.produce("led", "0")
+            return "LED OFF"
+        else:
+            return "Message not reconised"
 
-if __name__ == '__main__':
-    kafka_manager = KafkaManager("172.17.0.3")
-    sensors_bd = Sensors_BD('database.db', max=5000)
-    app.run(host='0.0.0.0', port=5000, debug=True)
