a11af803943b598315e54ed5f4e9713cd49eef96
GabyZ2027 <gabrie.gzt@gmail.com>
2024-05-27 20:29:35 +0200

    Corregido mqtt-kafka bridge y DataAcquisition Service

--- webapp-iot/app/src/services/DataAcquisitionService.py
+++ webapp-iot/app/src/services/DataAcquisitionService.py
@@ -1,8 +1,9 @@
-from multiprocessing import Process
+from threading import Thread
+import time
 
 TS = 3  # segundos
 
-class Data_Acquisition(Process):
+class Data_Acquisition(Thread):
     def __init__(self, kafka_manager, kafka_topics, request_queue):
         super().__init__()
         self.kafka_manager = kafka_manager
@@ -13,15 +14,20 @@ class Data_Acquisition(Process):
 
     def run(self):
         for topic in self.kafka_topics:
-            self.consumers.append(self.kafka_manager.create_consumer(topic))
-        while True:
-            for consumer in self.consumers:
-                for message in consumer:
-                    if message is not None:
-                        msg_payload = message.value.decode('utf-8')
-                        msg_data = msg_payload.split(',')
-                        print(f'Mensaje Kafka recibido en el topic {consumer._topic.name.decode()}: {msg_payload}')
-                        self.save_message_to_db(msg_data)
+            print(topic)
+            consumer = self.kafka_manager.create_consumer(topic)
+            self.consumers.append(consumer)
+            thread = Thread(target=self.consume_messages, args=(consumer,))
+            thread.start()
+
+    def consume_messages(self, consumer):
+        for message in consumer:
+            if message is not None:
+                msg_payload = message.value.decode('utf-8')
+                msg_data = msg_payload.split(',')
+                print(f'Mensaje Kafka recibido en el topic {consumer._topic.name.decode()}: {msg_payload}')
+                self.save_message_to_db(msg_data)
+                time.sleep(TS)
 
     def save_message_to_db(self, payload):
         # Crear una solicitud para guardar el mensaje en la base de datos
