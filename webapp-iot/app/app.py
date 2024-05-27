409f80470a955cadb01b9a92cb14ec40d98073b8
Marti <marti.rafart@estudiantat.upc.edu>
2024-05-27 19:00:26 +0200

    Actualitzat app.py, autentificador i afagit els certificats

--- webapp-iot/app/app.py
+++ webapp-iot/app/app.py
@@ -6,10 +6,20 @@ app=init_app()
 @app.route('/')
 def index():
     return redirect(url_for('auth_blueprint.login')) 
+    
+def status_401(error):
+    return redirect(url_for('auth_blueprint.login'))
+
+
+def status_404(error):
+    return "<h1>Page not found</h1>", 404
 
 if __name__ == '__main__':
     try:
-        app.run(debug=True,port=5600)
+    	context = ('certs/cert.pem', 'certs/key.pem')
+    	app.register_error_handler(401, status_401)
+        app.register_error_handler(404, status_404)
+    	app.run(debug=True,port=443, ssl_context=context) 
     except KeyboardInterrupt:
         print("Proceso interrumpido por el usuario.")
     finally:
--- webapp-iot/app/src/routes/AuthApi.py
+++ webapp-iot/app/src/routes/AuthApi.py
@@ -1,28 +1,70 @@
-from flask import Flask,Blueprint,render_template, request
+from flask import Flask,Blueprint,render_template, request, redirect, url_for, flash, session
 
 from config import Config
 
+from src.models.U_database import Usuaris_BD
+
 auth_blueprint = Blueprint('auth_blueprint',__name__)
+
+usuaris_BD= Usuaris_BD("usuaris.db")
+
+#@login_manager_app.user_loader
+"""
+@auth_blueprint.route('/')
+def index():
+    return redirect(url_for('auth_blueprint.login'))
+"""
+
+@auth_blueprint.route('/register', methods = ['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password == confirm_password:
+            if usuaris_BD.register(username,password):
+                flash('Registration successful.', 'success')
+                return redirect(url_for('auth_blueprint.login'))
+            else:
+                flash('Username already exists.', 'error')
+                return render_template('register.html')
+        else:
+            flash('Passwords do not match.', 'error')
+            return render_template('register.html')
+    else:
+        return render_template('register.html')
 
 @auth_blueprint.route('/login', methods = ['GET','POST'])
 def login():
     if request.method == 'POST':
-        print(request.form['email'])
-        print(request.form['password'])
-        return render_template('auth/login.html')
+        username = request.form['username']
+        password = request.form['password']
+        if usuaris_BD.login(username,password):
+            session['username'] = username
+            flash('Registration successful.', 'success')
+            return redirect(url_for('auth_blueprint.home'))
+        else:
+            flash('Username already exists.', 'error')
+            return render_template('login.html')
     else:
-        return render_template('auth/login.html')
-
-"""
-@auth_blueprint.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # Lógica de autenticación aquí
-        return redirect(url_for('api_blueprint.dashboard'))  # Redirigir a una página protegida después de iniciar sesión
-    return render_template('login.html')
+        return render_template('login.html')
 
 @auth_blueprint.route('/logout')
 def logout():
-    # Lógica de cierre de sesión aquí
+    session.pop('username', None)
+    flash('Logout successful.', 'success')
     return redirect(url_for('auth_blueprint.login'))
-"""
+    
+@auth_blueprint.route('/home')
+def home():
+    return render_template('home.html')
+
+def status_401(error):
+    return redirect(url_for('auth_blueprint.login'))
+
+
+def status_404(error):
+    return "<h1>Page not found</h1>", 404
+
+    
+    
