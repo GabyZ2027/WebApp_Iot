be25a6941c2c4666c579bf444bad1e42e1d72893
GabyZ2027 <gabrie.gzt@gmail.com>
2024-06-02 17:25:30 +0000

    arreglado logout

--- webapp-iot/app/config.py
+++ webapp-iot/app/config.py
@@ -3,7 +3,7 @@ class Config:
     DATABASE_NAME = 'sensors.db'
     DATABASE_MAX = 1000  # NUM REGISTROS
     MQTT_BROKER = "mqtt.eclipseprojects.io"
-    KAFKA_BROKER = '172.17.0.3:9092'
+    KAFKA_BROKER = 'localhost:9092'
     SENSOR_TOPICS = ['temperature_assi', 'led_assi', 'humitat_assi']
     ACTUATOR_TOPICS = 'led_act'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
