fa412980a832e588ce6fffaaf6051aa66848d2f8
GabyZ2027 <gabrie.gzt@gmail.com>
2024-06-03 18:43:27 +0200

    documentat kafka,mqtt-kafka bridge i data-Acq service

--- webapp-iot/app/src/models/kafka.py
+++ webapp-iot/app/src/models/kafka.py
@@ -2,6 +2,16 @@ from pykafka import KafkaClient
 from time import time
 
 class KafkaManager:
+    """
+    KafkaManager class for managing Kafka producers and consumers.
+
+    This class provides methods to create Kafka producers and consumers, and to produce and consume messages
+    from Kafka topics.
+
+    Args:
+        hosts (str): The address of the Kafka server in the format "host:port".
+    """
+
     def __init__(self, hosts):
         """
         Initializes the KafkaManager with the Kafka server's address.
@@ -41,13 +51,16 @@ class KafkaManager:
         consumer = topic.get_simple_consumer()
         return consumer
     
-    def consume_latest(self,consumer, timeout: float = 2.0):
+    def consume_latest(self, consumer, timeout: float = 2.0):
         """
-        Consume mensajes de Kafka hasta que se recibe uno nuevo o hasta que se alcanza el timeout.
-        
-        :param consumer: Un consumidor de Pykafka configurado.
-        :param timeout: Tiempo en segundos para esperar nuevos mensajes antes de detenerse.
-        :return: Una lista de mensajes consumidos.
+        Consumes messages from Kafka until a new message is received or the timeout is reached.
+
+        Args:
+            consumer (pykafka.simpleconsumer.SimpleConsumer): A configured Kafka consumer.
+            timeout (float): Time in seconds to wait for new messages before stopping.
+
+        Returns:
+            list: A list of consumed messages.
         """
         start_time = time()
         messages = []
@@ -55,34 +68,31 @@ class KafkaManager:
         for message in consumer:
             if message is not None:
                 messages.append(message.value.decode('utf-8'))
-                print("Mensaje consumido:", message.value.decode('utf-8'))
-                start_time = time() 
+                print("Consumed message:", message.value.decode('utf-8'))
+                start_time = time()
 
             if time() - start_time > timeout:
                 break
 
-        print("Fin del consumo")
+        print("End of consumption")
         return messages
-
 
     def consume(self, consumer):
         """
-        Consumes messages from the specified topic.
+        Consumes messages from the specified consumer.
 
         Args:
-            topic_name (str): The name of the topic from which the consumer will receive messages.
+            consumer (pykafka.simpleconsumer.SimpleConsumer): A configured Kafka consumer.
 
         Returns:
             list: A list of messages received from the topic.
         """
-        #consumer = self.create_consumer(topic_name)
         messages = []
         for message in consumer:
             if message is not None:
-                print("Mensaje consumido")
-                print(message.value.decode('utf-8'))
+                print("Consumed message:", message.value.decode('utf-8'))
                 messages.append(message.value.decode('utf-8'))
-        print("FI")
+        print("End of consumption")
         return messages
     
     def produce(self, producer, message):
@@ -90,7 +100,7 @@ class KafkaManager:
         Produces a message to the specified topic.
 
         Args:
-            topic_name (str): The name of the topic to which the producer will send the message.
+            producer (pykafka.producer.Producer): A configured Kafka producer.
             message (bytes): The message to be sent.
 
         Returns:
--- webapp-iot/mqtt_kafka_bridge.py
+++ webapp-iot/mqtt_kafka_bridge.py
@@ -4,7 +4,29 @@ from pykafka import KafkaClient
 from multiprocessing import Process
 
 class MQTTKafkaBridge:
+    """
+    MQTTKafkaBridge class to bridge messages between MQTT and Kafka.
+
+    This class is responsible for consuming messages from MQTT topics and publishing them to a Kafka topic,
+    and vice versa, enabling communication between MQTT and Kafka.
+
+    Args:
+        mqtt_broker (str): Address of the MQTT broker.
+        kafka_broker (str): Address of the Kafka broker.
+        mqtt_topics (list): List of MQTT topics to subscribe to.
+        kafka_topic (str): Kafka topic to publish messages to.
+    """
+
     def __init__(self, mqtt_broker, kafka_broker, mqtt_topics, kafka_topic):
+        """
+        Initializes the MQTTKafkaBridge instance.
+
+        Args:
+            mqtt_broker (str): Address of the MQTT broker.
+            kafka_broker (str): Address of the Kafka broker.
+            mqtt_topics (list): List of MQTT topics to subscribe to.
+            kafka_topic (str): Kafka topic to publish messages to.
+        """
         self.mqtt_broker = mqtt_broker
         self.kafka_broker = kafka_broker
         self.mqtt_topics = mqtt_topics
@@ -13,6 +35,9 @@ class MQTTKafkaBridge:
         self.kafka_mqtt_process = None
 
     def consume_kafka_publish_mqtt(self):
+        """
+        Consumes messages from a Kafka topic and publishes them to an MQTT topic.
+        """
         kafka_client = KafkaClient(hosts=self.kafka_broker)
         kafka_topic = kafka_client.topics[self.kafka_topic.encode('ascii')]
         kafka_consumer = kafka_topic.get_simple_consumer()
@@ -24,20 +49,30 @@ class MQTTKafkaBridge:
             if message is not None:
                 msg_payload = message.value.decode('ascii')
                 mqtt_client.publish(self.kafka_topic, msg_payload)
-                print(f'MQTT: Publicado {msg_payload} en el topic {self.kafka_topic}')
+                print(f'MQTT: Published {msg_payload} to topic {self.kafka_topic}')
 
     def on_message(self, client, userdata, message, kafka_producers):
+        """
+        Callback function to handle incoming MQTT messages and publish them to Kafka.
+
+        Args:
+            client (mqtt.Client): MQTT client instance.
+            userdata: User data (not used).
+            message (mqtt.MQTTMessage): Incoming MQTT message.
+            kafka_producers (dict): Dictionary of Kafka producers for each MQTT topic.
+        """
         msg_payload = str(message.payload.decode('utf-8'))
         mqtt_topic = message.topic
-        print(f'Mensaje MQTT recibido en el topic {mqtt_topic}: {msg_payload}')
+        print(f'MQTT message received on topic {mqtt_topic}: {msg_payload}')
         
         kafka_producer = kafka_producers[mqtt_topic]
-        print(kafka_producer)
         kafka_producer.produce(msg_payload.encode('ascii'))
-        print(f'KAFKA: Publicado {msg_payload} al topic {kafka_producer._topic.name.decode()}')
-
+        print(f'KAFKA: Published {msg_payload} to topic {kafka_producer._topic.name.decode()}')
 
     def consume_mqtt_publish_kafka(self):
+        """
+        Consumes messages from MQTT topics and publishes them to a Kafka topic.
+        """
         mqtt_client = mqtt.Client()
         mqtt_client.connect(self.mqtt_broker)
 
@@ -57,6 +92,9 @@ class MQTTKafkaBridge:
         mqtt_client.loop_forever()
 
     def start(self):
+        """
+        Starts the bridging processes between MQTT and Kafka.
+        """
         self.mqtt_kafka_process = Process(target=self.consume_mqtt_publish_kafka)
         self.kafka_mqtt_process = Process(target=self.consume_kafka_publish_mqtt)
 
@@ -67,7 +105,12 @@ class MQTTKafkaBridge:
         self.kafka_mqtt_process.join()
 
 if __name__ == "__main__":
-    mqtt_kafka_bridge = MQTTKafkaBridge("mqtt.eclipseprojects.io", "172.17.0.3:9092", ["temperature_assi", "led_assi","humitat_assi"], "led_act")
+    mqtt_kafka_bridge = MQTTKafkaBridge(
+        "mqtt.eclipseprojects.io", 
+        "172.17.0.3:9092", 
+        ["temperature_assi", "led_assi", "humitat_assi"], 
+        "led_act"
+    )
 
     mqtt_kafka_process = Process(target=mqtt_kafka_bridge.consume_mqtt_publish_kafka)
     kafka_mqtt_process = Process(target=mqtt_kafka_bridge.consume_kafka_publish_mqtt)
