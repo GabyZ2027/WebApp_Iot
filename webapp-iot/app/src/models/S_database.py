52ffa68db5e3b14aae4f5b4fc2eb37077713cbc9
RubenHerRui <rubher2003@gmail.com>
2024-06-03 17:52:13 +0200

    Base de dades documentada

--- webapp-iot/app/src/models/S_database.py
+++ webapp-iot/app/src/models/S_database.py
@@ -1,24 +1,25 @@
 import sqlite3
 import sys
-
-#Utilització:
-#Crear la BD
-#max = 5000
-#nom1 = "sensors.db"
-#sensors = SBD.Sensors_BD(nom1,max)
-#Utilitzar les id:
-#Els metodes et demanane un id, cada id correspon a un sensor/actuador pactem:
-#Actudador(LED) = 0
-#Sensor(Temperatura) = 1
-#Sensor(Humitat) = 2
 from time import sleep
 from multiprocessing import Process, Queue
 from queue import Empty
 import sqlite3
 import bcrypt
 
+"""
+classe DatabaseServer
+"""
 class DatabaseServer(Process):
     def __init__(self, db_name, max_size, request_queues, response_queues):
+        """
+        Initializes the DatabaseServer class.
+
+        Args:
+            db_name (str): The name of the .db file that stores all the modifications. It should be 'sensors.db'.
+            max_size (int): Sets the maximum number of records for the 'sensors' table.
+            request_queues (dict): Specifies the dictionary of queues where request orders to the database are received.
+            response_queues (dict): Specifies the dictionary of queues where return values from requests are saved.
+        """
         super().__init__()
         self.db_name = db_name
         self.max_size = max_size
@@ -28,6 +29,10 @@ class DatabaseServer(Process):
         self.db_cursor = None
 
     def run(self):
+        """
+        Establishes a connection to the database, creates required tables if they don't exist,
+        and continuously handles requests received from request queues.
+        """
         try:
             self.db_connection = sqlite3.connect(self.db_name)
             self.db_cursor = self.db_connection.cursor()
@@ -61,6 +66,13 @@ class DatabaseServer(Process):
             print("Error %s:" % e.args[0])
 
     def handle_request(self, request):
+        """
+        Handles different types of requests received from the request queues.
+        Args:
+            request (dict): A dictionary representing a request.
+        Returns:
+            Various: Returns values based on the type of request.
+        """
         if request['type'] == 'getLectura':
             return self.get_lectura(request['id'])
         elif request['type'] == 'getLectures':
@@ -75,6 +87,18 @@ class DatabaseServer(Process):
             return self.getUser(request['username'])
 
     def get_lectura(self, id):
+        """
+        Retrieves the most recent value and date from the database for the specified ID.
+        Args:
+            id (int): Represents the ID associated with a sensor or actuator.
+                - ID = 0: LED
+                - ID = 1: Temperature sensor
+                - ID = 2: Humidity sensor
+        Returns:
+            tuple: A tuple containing the ID, value, and date if found, otherwise None.
+        Raises:
+            sqlite3.Error: If an error occurs during the database query.
+        """
         try:
             self.db_cursor.execute(
                 "SELECT LECTURA, DATA FROM SENSORS WHERE ID = ? ORDER BY DATA DESC LIMIT 1",
@@ -87,6 +111,20 @@ class DatabaseServer(Process):
             return None
 
     def get_lectures(self, id, quantitat):
+        """
+        Retrieves the specified number of most recent values from the database for the given ID.
+        Args:
+            id (int): Represents the ID associated with a sensor or actuator.
+                - ID = 0: LED
+                - ID = 1: Temperature sensor
+                - ID = 2: Humidity sensor
+            quantitat (int): The number of most recent values to retrieve.
+
+        Returns:
+            tuple: A tuple containing a string identifier and a list of readings.
+        Raises:
+            sqlite3.Error: If an error occurs during the database query.
+        """
         try:
             self.db_cursor.execute(
                 "SELECT LECTURA, DATA FROM SENSORS WHERE ID = ? ORDER BY DATA DESC LIMIT ?",
@@ -99,6 +137,19 @@ class DatabaseServer(Process):
             return []
 
     def set_lectura(self, id, lectura):
+        """
+        Inserts a value into the database in the SENSORS table associated with the specified ID and the current time. If the maximum number of records is surpassed, the oldest one is deleted.
+        Args:
+            id (int): Represents the ID associated with a sensor or actuator.
+                - ID = 0: LED
+                - ID = 1: Temperature sensor
+                - ID = 2: Humidity sensor
+            lectura (str): The value read from a sensor or actuator.
+        Returns:
+            bool: True if the insertion is successful, False otherwise.
+        Raises:
+            sqlite3.Error: If an error occurs during the database operation.
+        """
         try:
             self.db_cursor.execute("SELECT COUNT(*) FROM SENSORS")
             quant = self.db_cursor.fetchone()[0]
@@ -116,11 +167,38 @@ class DatabaseServer(Process):
             self.db_connection.rollback()
             print("Error %s:" % e.args[0])
             return False
+        
+    def getUser(self, name):
+        """
+        Checks if the username already exists in the database.
+        Args:
+            name (str): The username to check.
+        Returns:
+            bool: True if the username exists, False otherwise.
+        Raises:
+            sqlite3.Error: If an error occurs during the database query.
+        """
+        try:
+            self.db_cursor.execute("SELECT NOM FROM USERS WHERE NOM = ?", (name,))
+            lectura = self.db_cursor.fetchone()
+            return True if lectura else False    
+        except sqlite3.Error as e:
+            self.__con.rollback()
+            print("Error %s:" % e.args[0])
 
     def register_user(self, username, password):
+        """
+        Registers a new user in the database with the specified username and password hash (bcrypt).
+        Args:
+            username (str): The username to register.
+            password (str): The password to hash and store.
+        Returns:
+            bool: True if the user is registered successfully, False if the username already exists.
+        Raises:
+            sqlite3.Error: If an error occurs during the database operation.
+        """
         try:
-            self.db_cursor.execute("SELECT NOM FROM USERS WHERE NOM = ?", (username,))
-            if self.db_cursor.fetchone() is None:
+            if self.getUser(username) is False:
                 hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
                 self.db_cursor.execute("INSERT INTO USERS (NOM, PASSW) VALUES (?, ?)", (username, hashed_password))
                 self.db_connection.commit()
@@ -131,17 +209,18 @@ class DatabaseServer(Process):
             self.db_connection.rollback()
             print("Error %s:" % e.args[0])
             return False
-    
-    def getUser(self, name):
-        try:
-            self.db_cursor.execute("SELECT NOM FROM USERS WHERE NOM = ?", (name,))
-            lectura = self.db_cursor.fetchone()
-            return True if lectura else False    
-        except sqlite3.Error as e:
-            self.__con.rollback()
-            print("Error %s:" % e.args[0])
 
     def login_user(self, name, passw):
+        """
+        Authenticates a user by verifying the provided password against the stored hash.
+        Args:
+            name (str): The username to authenticate.
+            passw (str): The password to verify.
+        Returns:
+            bool or None: True if the authentication is successful, False if the password is incorrect, None if the username does not exist.
+        Raises:
+            sqlite3.Error: If an error occurs during the database query.
+        """
         try:
             if self.getUser(name) == True:
                 self.db_cursor.execute("SELECT PASSW FROM USERS WHERE NOM = ?", (name,))
@@ -149,21 +228,26 @@ class DatabaseServer(Process):
                 hash = lectura[0]
                 epassw = passw.encode()
                 if bcrypt.checkpw(epassw,hash):
-                    return True     #S'ha verificat
+                    return True     
                 else:
-                    return False    #Passwd incorrecte
+                    return False    
             else:  
-                return None         #No existeix l'usuari
+                return None         
         except sqlite3.Error as e:
             self.__con.rollback()
             print("Error %s:" % e.args[0])
 
     def close_connection(self):
+        """
+        Closes the connection to the database.
+        Returns:
+            None
+        """
         if self.db_connection:
             self.db_connection.close()
 
 
-
+"""
 def main():
     request_queue = Queue()
     response_queue = Queue()
@@ -173,7 +257,6 @@ def main():
 
     # Esperar a que el servidor de base de datos se inicie
     sleep(1)
-    """
     # Test setLectura
     request_queue.put({'type': 'setLectura', 'id': 1, 'lectura': '25.0'})
     sleep(3)  # Esperar a que la base de datos procese la solicitud
@@ -192,7 +275,6 @@ def main():
         sleep(3)  # Esperar a que la base de datos procese la solicitud
         response = response_queue.get()
         print(f'setLectura response: {response}')
-    """
     # Test getLectures
     request_queue.put({'type': 'getLectures', 'id': 0, 'quantitat': 10})
     sleep(3)  # Esperar a que la base de datos procese la solicitud
@@ -205,7 +287,7 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+"""
 
 """
 # Ahora puedes usar esta clase `DatabaseServer` como servidor de base de datos
